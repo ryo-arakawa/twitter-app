4778e083acff4960641366d5651a16aa30112a80
Ryo Arakawa <67402912+ryo-arakawa@users.noreply.github.com>
2021-05-14 14:41:21 +0900

    Gooogleアカウント認証

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { selectUser, login, logout } from "./features/userSlice";
 import { auth } from "./firebase";
 import { Feed } from "./components/Feed";
-import { Auth } from "./components/Auth";
+import Auth from "./components/Auth";
 
 const App: React.FC = () => {
   const user = useSelector(selectUser);
