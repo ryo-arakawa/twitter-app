4a4f5c4140ad8e3aae770321f1afd2781295946e
Ryo Arakawa <67402912+ryo-arakawa@users.noreply.github.com>
2021-05-16 01:49:10 +0900

    Email+Password情報認証

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from "react";
 import { auth } from "../firebase";
 export const Feed = () => {
   return (
     <div>
       Feeds
+      <button onClick={() => auth.signOut()}>Logout</button>
     </div>
-  )
-}
+  );
+};
 
+export default Feed;
