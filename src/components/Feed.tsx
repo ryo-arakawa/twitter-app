4f6bfdf3571a70bdbb03438c24802cde2d42c2f0
Ryo Arakawa <67402912+ryo-arakawa@users.noreply.github.com>
2021-05-19 15:33:42 +0900

    ログインユーザーアバター

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { auth } from "../firebase";
+import TweetInput from "./TweetInput";
+
 export const Feed = () => {
   return (
     <div>
-      Feeds
+      Feed
+      <TweetInput />
       <button onClick={() => auth.signOut()}>Logout</button>
     </div>
   );
