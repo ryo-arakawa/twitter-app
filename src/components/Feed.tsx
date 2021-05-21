f9bd674dcdc401b5d1203c4d19a3babc9cdc1e78
Ryo Arakawa <67402912+ryo-arakawa@users.noreply.github.com>
2021-05-21 17:55:50 +0900

    Feed機能

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,15 +1,47 @@
+import React, { useState, useEffect } from "react";
 import { StylesProvider } from "@material-ui/styles";
 import styles from "./Feed.module.css";
-import React from "react";
-import { auth } from "../firebase";
+import { db } from "../firebase";
 import TweetInput from "./TweetInput";
 
-export const Feed = () => {
+const Feed: React.FC = () => {
+  const [posts, setPosts] = useState([
+    {
+      id: "",
+      avatar: "",
+      image: "",
+      text: "",
+      timestamp: null,
+      username: "",
+    },
+  ]);
+  useEffect(() => {
+    const unSub = db
+      .collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) =>
+        setPosts(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            avatar: doc.data().avatar,
+            image: doc.data().image,
+            text: doc.data().text,
+            timestamp: doc.data().timestamp,
+            username: doc.data().username,
+          }))
+        )
+      );
+    return () => {
+      unSub();
+    };
+  }, []);
+  
   return (
     <div className={styles.feed}>
-      Feed
       <TweetInput />
-      <button onClick={() => auth.signOut()}>Logout</button>
+      {posts.map((post) => (
+        <h3>{post.id}</h3>
+      ))}
     </div>
   );
 };
